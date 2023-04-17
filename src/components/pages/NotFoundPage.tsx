8d4f2c04da9fc9e898cb58d55dd40fdf38df92db
River <ahe794613@gmail.com>
2023-04-17 17:12:08 +0800

    404 page

---
+++ src/components/pages/NotFoundPage.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Image from 'next/image'
+import { useRouter } from 'next/navigation'
+import { MdKeyboardReturn } from 'react-icons/md'
+import { Box, Button } from '@chakra-ui/react'
+
+export const NotFoundPage = () => {
+  const router = useRouter()
+
+  const handleGoHome = () => {
+    router.push('/')
+  }
+
+  return (
+    <main className="flex w-full flex-col items-center bg-white pb-4">
+      <Box className="relative aspect-square w-3/5">
+        <Image alt="404" src="/404.jpg" fill className="object-contain" />
+      </Box>
+      <Button
+        onClick={handleGoHome}
+        rightIcon={<MdKeyboardReturn />}
+        className="border-2 text-2xl"
+      >
+        Go to Home Page
+      </Button>
+    </main>
+  )
+}
--- src/components/pages/index.ts
+++ src/components/pages/index.ts
@@ -1,4 +1,5 @@
 'use client'
 export * from './LoginPage'
+export * from './NotFoundPage'
 export * from './ProductsPage'
 export * from './OverviewPage'
